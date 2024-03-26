4a31c2b7adf422682dca65155de5ff54321730c4
chudistar01 <christianigwebuike31@gmail.com>
2024-03-25 21:26:17 -0700

    Airbnb Project

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+city class
+"""
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """
+    amenity
+    """
+
+    state_id = ""
+    name = ""
---
+++ tests/test_models/test_city.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Test
+"""
+import os
+import models
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+class TestCity(unittest.TestCase):
+    """
+    Test
+    """
+
+    def test_name(self):
+        """
+        test
+        """
+        pass
+
+if __name__ = '__main__':
+    unittest.main()
